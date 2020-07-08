8807293bf7a7d24e0bba277cbab8778dbe00a810
akshay <tvibhu12@gmail.com>
2020-07-08 20:50:42 +0530

    add navbar animation

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//navbar start
 document.getElementById('toggle').addEventListener('click',()=>{
     document.getElementById('toggle').classList.toggle("on")
     document.getElementById('resize').classList.toggle("active")
@@ -12,3 +13,18 @@ document.querySelector('.close-btn').addEventListener('click',()=>{
     document.querySelector('.close-btn').classList.toggle("on")
     document.getElementById('resize').classList.toggle("active")
 })
+//navbar end
+//navbar animation
+TweenMax.from('#brand',1,{
+    delay:0.4,
+    y:10,
+    opacity:0,
+    ease:Expo.easeInOut
+})
+TweenMax.staggerFrom('#menu li a',1,{
+    delay:0.4,
+   
+    opacity:0,
+    ease:Expo.easeInOut
+},0.1)
+//navbar animation end
